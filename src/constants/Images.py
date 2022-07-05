cc1a7a9e79fdcf94de535ebf5a2ea640edc2d9a4
Admin <Admin@DESKTOP-05LT3I1>
2022-07-05 13:41:19 +0300

    Directory cleanup

--- src/constants/Images.py
+++ src/constants/Images.py
@@ -3,21 +3,21 @@ from src.constants.RaobfNames import *
 from src.lib.jsDictionary import JSDictionary
 from src.lib.fixedASRImage import FixedASRImage
 
-buttons = Atlas("src/data/buttons.atlas")
+buttons = Atlas("src/art/buttons.atlas")
 
 IMAGES = JSDictionary({
     "ATTACK_DISC" : JSDictionary({
-        "RB"   : "src/data/FrontSide/{}.png".format("_".join(RB.split(" "))),
-        "CR"   : "src/data/FrontSide/{}.png".format("_".join(CR.split(" "))),
-        "TD"   : "src/data/FrontSide/{}.png".format("_".join(TD.split(" "))),
-        "BALA" : "src/data/FrontSide/{}.png".format("_".join(BALA.split(" "))),
-        "ACP"  : "src/data/FrontSide/{}.png".format("_".join(ACP.split(" ")))
+        "RB"   : "src/art/FrontSide/{}.png".format("_".join(RB.split(" "))),
+        "CR"   : "src/art/FrontSide/{}.png".format("_".join(CR.split(" "))),
+        "TD"   : "src/art/FrontSide/{}.png".format("_".join(TD.split(" "))),
+        "BALA" : "src/art/FrontSide/{}.png".format("_".join(BALA.split(" "))),
+        "ACP"  : "src/art/FrontSide/{}.png".format("_".join(ACP.split(" ")))
     }),
 
     "SLIDE_RULE_DISC" : JSDictionary({
-        "AOB"  : "src/data/BackSide/AOB_Disc.png",
-        "DS"   : "src/data/BackSide/Distance_Speed_Disc.png",
-        "TM"   : "src/data/BackSide/{}.png".format("_".join(TM.split(" ")))
+        "AOB"  : "src/art/BackSide/AOB_Disc.png",
+        "DS"   : "src/art/BackSide/Distance_Speed_Disc.png",
+        "TM"   : "src/art/BackSide/{}.png".format("_".join(TM.split(" ")))
     }),
 
     "BUTTONS" : JSDictionary({
